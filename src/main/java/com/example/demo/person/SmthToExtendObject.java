a29c7ab2b9cbe2473729bc1448ea729f3cc9322d
Timur <timk01@mail.ru>
2022-10-31 22:47:18 +0300

    Interview, OOP stage

---
+++ src/main/java/com/example/demo/person/SmthToExtendObject.java
@@ -0,0 +1,12 @@
+package com.example.demo.person;
+
+public class SmthToExtendObject extends Object {
+    public static void main(String[] args) {
+        System.out.println("yes, i really explicitly extend object. why ?");
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return false;
+    }
+}
